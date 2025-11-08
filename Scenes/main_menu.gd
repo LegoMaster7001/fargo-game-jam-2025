749512fa98726d18433deb8ff35e6dee6045f9a1
Noah Magcalas <noah.magcalas@gmail.com>
2025-11-08 17:44:43 -0600

    functional main menu

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -7,8 +7,15 @@ var _current_animation_idx := 0
 var _animations := ["chase_left", "chase_right"]
 
 func _ready() -> void:
+	Engine.time_scale = 0
 	animation_player.animation_finished.connect(play_next_animation)
 	animation_player.play(_animations[_current_animation_idx])
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if not event.is_pressed():
+		return
+	hide()
+	Engine.time_scale = 1
 
 func get_next_animation_idx() -> int:
 	return (_current_animation_idx + 1) % _animations.size()
