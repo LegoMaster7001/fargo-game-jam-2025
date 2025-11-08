acda341a73c214e50b8d3e8aac77aa61526629f8
Noah Magcalas <noah.magcalas@gmail.com>
2025-11-08 13:29:13 -0600

    add time limit for hunting & corresponding hud element

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,13 +1,26 @@
 extends Node2D
 
+const CHASE_DURATION := 10
+
 signal role_changed(hunted: bool)
 
 var hunted = false
-
 # "alias" for !hunted
 var hunting: bool :
 	get(): return !hunted
 
+var chase_timer := Timer.new()
+
+func _ready() -> void:
+	chase_timer.wait_time = CHASE_DURATION
+	chase_timer.timeout.connect(_on_timeout)
+	add_child(chase_timer)
+
 func flip_role():
 	Global.hunted = !Global.hunted
 	Global.role_changed.emit(Global.hunted)
+	chase_timer.start()
+
+func _on_timeout() -> void:
+	# TODO: deduct a point
+	Global.flip_role()
--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,11 +1,17 @@
 class_name HUD
 extends Control
 
+const TIME_FORMAT = "%.1f"
+
 @export var role_label: Label
+@export var time_label: Label
 
 func _ready() -> void:
 	_on_role_changed(Global.hunted)
 	Global.role_changed.connect(_on_role_changed)
+
+func _physics_process(delta: float) -> void:
+	time_label.text = TIME_FORMAT % [Global.chase_timer.time_left]
 
 func _on_role_changed(hunted: bool) -> void:
 	# note this is the enemy's role
