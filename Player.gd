e4d762e8262449bba993977a5908709025968867
Gannon Andrews <superminr21@gmail.com>
2025-11-07 20:35:41 -0600

    Revert "Feat/groundies"

--- Player.gd
+++ Player.gd
@@ -1,9 +1,6 @@
 extends CharacterBody2D
 
 const speed = 100
-
-@export var enemy: Enemy
-@export var groundies_area: GroundiesArea
 var dir : Vector2
 
 func _physics_process(delta: float):
@@ -14,9 +11,3 @@ func _unhandled_input(event: InputEvent):
 	dir.x = Input.get_axis("ui_left", "ui_right")
 	dir.y = Input.get_axis("ui_up", "ui_down")
 	dir = dir.normalized()
-
-	if event.is_action_pressed("action"):
-		groundies_area.call_groundies()
-
-	if event.is_action_pressed("debug_flip_role"):
-		enemy.flip_role()
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,34 +1,36 @@
-class_name Enemy
 extends CharacterBody2D
 
 const speed = 20
 
 @export var player: Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
-@onready var initial_pos := global_position
-var hunted = true
+var test = true
 
 func _physics_process(delta: float) -> void:
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
 	velocity = dir * speed
 	move_and_slide()
-	if (hunted):
+	if (test):
 		$Sprite2D.modulate = Color(0, 1, 0)
 	else:
 		$Sprite2D.modulate = Color(1, 0, 0)
 
 func makepath():
 	nav_agent.target_position = player.global_position
-	if (hunted):
+	if (test):
 		nav_agent.target_position = -player.global_position
 
-func flip_role():
-	hunted = !hunted
+
+func _on_timer_timeout():
 	makepath()
 
-func _on_pathfinding_timer_timeout() -> void:
-	makepath()
 
-func _on_role_flip_timer_timeout() -> void:
-	flip_role()
+func _on_timer_2_timeout() -> void:
+	test = !test
 
+func _on_tag_zone_body_entered(body: Node2D) -> void:
+	print(body)
+	if(test):
+		print("You tagged me!")
+	else:
+		print("I tagged you!")
