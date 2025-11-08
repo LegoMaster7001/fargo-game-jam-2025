c0051794688c6d10493afbbd2f949909763c25c7
Noah Magcalas <noah.magcalas@gmail.com>
2025-11-08 14:19:01 -0600

    add global timer to limit chase duration

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -13,13 +13,13 @@ var chase_timer := Timer.new()
 
 func _ready() -> void:
 	chase_timer.wait_time = CHASE_DURATION
+	chase_timer.autostart = true
 	chase_timer.timeout.connect(_on_timeout)
 	add_child(chase_timer)
 
 func flip_role():
 	Global.hunted = !Global.hunted
 	Global.role_changed.emit(Global.hunted)
-	chase_timer.start()
 
 func _on_timeout() -> void:
 	# TODO: deduct a point
