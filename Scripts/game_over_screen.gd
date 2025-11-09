2be7544db523f3d6f5b2838e152a7f2c34524734
Noah Magcalas <noah.magcalas@gmail.com>
2025-11-09 16:49:41 -0600

    different music for win/loss

--- Scripts/game_over_screen.gd
+++ Scripts/game_over_screen.gd
@@ -10,15 +10,18 @@ const LOSE_TEXT := "(you [color=red]LOST...[/color])"
 
 @onready var _title_initial_pos := title.position
 
-func animate():
-	victory_label.text = WIN_TEXT if Global.score > 0 else LOSE_TEXT
+func do_game_over_stuff():
+	var won = Global.score > 0
+	MusicPlayer.stream = MusicPlayer.WIN_MUSIC if won else MusicPlayer.LOSS_MUSIC
+	MusicPlayer.play()
+	victory_label.text = WIN_TEXT if won else LOSE_TEXT
 
 	var tween = create_tween().set_parallel(true)
 
 	tween.tween_method(set_mix_amount, 0.0, 0.8, 0.5)
 	tween.tween_method(set_blur_amount, 0.0, 1, 0.5)
 
-	var title_color := Color.GREEN if Global.score > 0 else Color.RED
+	var title_color := Color.GREEN if won else Color.RED
 	var title_start_pos := _title_initial_pos + Vector2(0, -get_viewport_rect().size.y)
 	tween.tween_property(title, "position", _title_initial_pos, 0.3).from(title_start_pos)
 	tween.tween_property(title, "modulate", title_color, 0.8).from(Color.TRANSPARENT)
