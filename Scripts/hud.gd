dc5faf8c64d6e785f530e0ff3adf4ca125e7a708
Noah Magcalas <noah.magcalas@gmail.com>
2025-11-08 18:54:14 -0600

    fix: actually call compass.update_needle

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -15,11 +15,14 @@ var isInArea = false
 
 func _ready() -> void:
 	update_text()
+	update_compass_visibity()
 	Global.role_changed.connect(_on_role_changed)
 	groundiesArea.groundiesCalled.connect(_on_groundies_called)
 
 func _physics_process(delta: float) -> void:
 	time_label.text = TIME_FORMAT % [Global.chase_timer.time_left]
+	var player_to_enemy := player.position.direction_to(enemy.position)
+	compass.update_needle(player_to_enemy, delta)
 
 func _on_groundies_called(isInArea):
 	print(isInArea)
@@ -38,6 +41,10 @@ func _on_role_changed(old: Global.role, current: Global.role, timeout: bool) -> 
 	elif (Global.role.hunter == old && timeout):
 		Global.subtractScore()
 	update_text()
+	update_compass_visibity()
+
+func update_compass_visibity():
+	compass.visible = Global.player_is_hunter
 
 func update_text():
 	var text = "HUNT." if Global.player_is_hunter else "HIDE!"
